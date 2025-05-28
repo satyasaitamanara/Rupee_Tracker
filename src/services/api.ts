377e724d6c2eb1f69fb56b19f544d91fc0702692
satyasaitamanara <tamanarasatyasai@gmail.com>
2025-05-28 09:30:10 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://rupee-tracker.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
